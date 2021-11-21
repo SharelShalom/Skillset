bd9bfce45ddb82cdfd74a9d396229b2103697bb6
Sharel Shalom <sharel1155@gmail.com>
2021-11-21 21:35:23 +0200

    Styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import RenderTabs from './renderForm/RenderTabs';
 import './App.css';
+import pages from './data/HomeTest'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RenderTabs tabs={pages.simulation.children}/>
     </div>
   );
 }
